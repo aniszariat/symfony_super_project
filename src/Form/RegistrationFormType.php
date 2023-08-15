c7732e362f67bb08e14cb062fe8810a120205039
aniszariat <zariatanis.az@gmail.com>
2023-08-15 16:25:34 +0100

    making authentication system

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,12 +19,24 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('city')
-            ->add('zipcode')
-            ->add('address')
+            ->add('email', EmailType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'E-mail'])
+            ->add('firstname', TextType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'Prénom'])
+            ->add('lastname', TextType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'Nom'])
+            ->add('city', TextType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'Ville'])
+            ->add('zipcode', TextType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'Code Postal'])
+            ->add('address', TextType::class, ['attr'=>[
+                'class'=>'form-control'
+            ], 'label'=>'adresse'])
             // ->add('agreeTerms', CheckboxType::class, [
             ->add('RGPDConsent', CheckboxType::class, [
                 'mapped' => false,
@@ -31,12 +45,13 @@ class RegistrationFormType extends AbstractType
                         'message' => 'You should agree to our terms.',
                     ]),
                 ],
+                'label'=>'j\'accepte les conditions d\'utilisation'
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password','class'=>'form-control'],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
--- src/Security/UserAuthenticator.php
+++ src/Security/UserAuthenticator.php
@@ -49,7 +49,7 @@ class UserAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('main'));
+        return new RedirectResponse($this->urlGenerator->generate('app_main'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
