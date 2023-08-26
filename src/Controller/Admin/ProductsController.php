a071c46853778d6228dca20ce871d7235a4f4c42
aniszariat <zariatanis.az@gmail.com>
2023-08-26 16:40:38 +0100

    creation des formulairs d'ajout de produits

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Products;
+use App\Form\ProdcutsFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response ;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/products', name:'app_admin')]
 class ProductsController extends AbstractController
@@ -17,16 +21,68 @@ class ProductsController extends AbstractController
         return $this->render('admin/products/index.html.twig');
     }
     #[Route('/add', name:'_add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): Response
     {
-        return $this->render('admin/products/index.html.twig');
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        // * on cre le produit
+        $product = new Products();
+
+        // * on cre le formulaire
+        $productForm = $this->createForm(ProdcutsFormType::class, $product);
+
+        // * ontraite la requete
+        $productForm->handleRequest($request);
+
+        // if ($productForm->isSubmitted() && $productForm->isValid()) {
+        if ($productForm->isSubmitted()) {
+            // dd($productForm);
+
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+            $product->setPrice($product->getPrice() * 100);
+            $manager->persist($product);
+            $manager->flush();
+            $this->addFlash('success', 'Produit ajouté avec succes');
+            return $this->redirectToRoute('app_admin_products');
+        }
+        return $this->render('admin/products/add.html.twig', compact(
+            'productForm'
+        ));
+        // return $this->render('admin/products/add.html.twig', [
+        //         'productForm' => $productForm->createView()
+        // ]);
+        // return $this->renderForm('admin/products/add.html.twig', compact('productForm'));
     }
     #[Route('/edit/{id}', name:'_edit')]
-    public function edit(Products $product): Response
+    public function edit(Request $request, SluggerInterface $slugger, EntityManagerInterface $manager, Products $product): Response
     {
         // * on verfie si le user peut editer aved voter
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
-        return $this->render('admin/products/index.html.twig');
+
+
+
+        // * on cre le formulaire
+        $productForm = $this->createForm(ProdcutsFormType::class, $product);
+
+
+        // * ontraite la requete
+        $productForm->handleRequest($request);
+        $product->setPrice($product->getPrice() / 100);
+        // if ($productForm->isSubmitted() && $productForm->isValid()) {
+        if ($productForm->isSubmitted()) {
+            // dd($productForm);
+
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+            $product->setPrice($product->getPrice() * 100);
+            $manager->persist($product);
+            $manager->flush();
+            $this->addFlash('success', 'Produit modifié avec succes');
+            return $this->redirectToRoute('app_admin_products');
+        }
+
+
+        return $this->render('admin/products/edit.html.twig', compact('productForm'));
     }
     #[Route('/delete/{id}', name:'_delete')]
     public function delete(Products $product): Response
--- src/Form/ProdcutsFormType.php
+++ src/Form/ProdcutsFormType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Categories;
 use App\Entity\Products;
+use App\Repository\CategoriesRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,13 +15,29 @@ class ProdcutsFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('stock')
-            ->add('created_at')
-            ->add('slug')
-            ->add('categories')
+            ->add('name', options:[
+                'label' => 'Nom'
+            ])
+            ->add('description', options:[
+                'label' => 'Description'
+            ])
+            ->add('price', options:[
+                'label' => 'Prix'
+            ])
+            ->add('stock', options:[
+                'label' => 'Unités en Stock'
+            ])
+            ->add('categories', EntityType::class, options:[
+                'class' => Categories::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'group_by' => 'parent.name',
+                'query_builder' => function (CategoriesRepository $cr) {
+                    return $cr->createQueryBuilder('c')
+                    ->where('c.parent IS NOT NULL')
+                    ->orderBy('c.name', 'ASC');
+                }
+            ])
         ;
     }
 
